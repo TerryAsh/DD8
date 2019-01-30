3345b0e75c4202c31b10bc707e659438cff0e29d
陈震 <chenzhen@jiaddeMacBook-Pro.local>
2019-01-30 13:53:23 +0800

    cell and network

--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,22 +1,68 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Button} from 'react-native';
+import {TouchableOpacity,FlatList ,Platform, StyleSheet, Text, View,Button} from 'react-native';
 
 export default class HomePage extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
   };
-  render() {
-  	const {navigate} = this.props.navigation;
 
+  constructor(props){
+    super(props);
+    this.state = {cloths:["xxx","22"]};
+    this.getCloths();
+  }
+
+  async getCloths(){
+    try{
+      let response = await fetch("https://suggest.taobao.com/sug?code=utf-8&q=%E5%8D%AB%E8%A1%A3", {
+        method: "GET",
+      });
+      let responseJson = await response.json();
+      this.setState(preState =>{
+        let cloths = responseJson["result"];
+        preState.cloths = cloths;
+        return preState;
+      });
+    } catch(error){
+      console.error(error);
+    }
+  }
+
+  render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Page</Text>
-        <Button
-        	title="Go to Score"
-        	onPress={() => navigate('Score', {name: 'Jane'})}
-      	/>
+        <FlatList data = {this.state.cloths} 
+                  renderItem={({item}) => {
+                  return <HomeListCell item = {item} navigation={this.props.navigation}/>;
+                  }}
+        />
       </View>
     );
   }
 }
+
+class HomeListCell extends React.PureComponent{
+
+  constructor(props){
+    super(props);
+  }
+
+  _handlePressedItem = ()=>{
+    const {navigate} = this.props.navigation;
+    navigate('Score', {name: this.props.item});
+  };
+
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TouchableOpacity onPress={this._handlePressedItem}> 
+            <Text> {this.props.item} </Text>
+         </TouchableOpacity>
+      </View>
+    );
+  }
+
+}
+
+
