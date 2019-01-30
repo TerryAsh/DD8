7084f2b335e005ddb3043bb30fc8ad8b9516941e
陈震 <chenzhen@jiaddeMacBook-Pro.local>
2019-01-30 09:39:10 +0800

    props and state

--- Pages/ScorePage.js
+++ Pages/ScorePage.js
@@ -3,12 +3,40 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class ScorePage extends React.Component {
+  static navigationOptions = {
+    title: 'Score',
+  };
   render() {
+  	const navigation = this.props.navigation;
+  	let username = navigation.getParam('name','defaultName1');
+
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Score Page</Text>
-
+        <Blink name = {username}/>
       </View>
     );
   }
+ }
+
+
+ class Blink extends React.Component{
+ 	constructor(props){
+ 		super(props);
+ 		this.state = {'hidden':true};
+ 		setInterval(()=>{
+ 			this.setState(preState =>{
+ 				return {'hidden':!preState.hidden};
+ 			});
+ 		},1000);
+ 	}
+
+ 	render(){
+ 			if (this.state.hidden) {
+ 				return null;
+ 			} else {
+ 				return (
+ 					<Text>over here {this.props.name}</Text>
+ 				);
+ 			}
+ 	}
  }
